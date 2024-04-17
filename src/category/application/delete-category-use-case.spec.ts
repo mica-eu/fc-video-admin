cab808cd50d30c8e653270e6ccf569f6e20f41ef
Micael Lima <5789811+mica-eu@users.noreply.github.com>
2024-04-16 23:27:26 -0300

    feat: add DeleteCategoryUseCase

---
+++ src/category/application/delete-category-use-case.spec.ts
@@ -0,0 +1,29 @@
+import { UUID } from "../../shared/domain/value-object/uuid.value-object";
+import { Category } from "../domain/category-entity";
+import { ICategoryRepository } from "../domain/category-repository";
+import { InMemoryCategoryRepository } from "../infra/db/in-memory/in-memory-category-repository";
+import { DeleteCategoryUseCase } from "./delete-category-use-case";
+
+describe("DeleteCategoryUseCase", () => {
+  let useCase: DeleteCategoryUseCase;
+  let categoryRepository: ICategoryRepository;
+
+  beforeEach(() => {
+    categoryRepository = new InMemoryCategoryRepository();
+    useCase = new DeleteCategoryUseCase(categoryRepository);
+  });
+
+  it("deletes a category", async () => {
+    jest.spyOn(categoryRepository, "delete");
+    const category = Category.create({ name: "Category Name" });
+    await categoryRepository.insert(category);
+    await useCase.execute(category.id.value);
+    expect(categoryRepository.delete).toHaveBeenCalledWith(category.id);
+  });
+
+  it("throws an error if the category does not exist", async () => {
+    await expect(
+      useCase.execute("db07e561-2738-4e20-acaa-69a432ddd8b2")
+    ).rejects.toThrow("Entity not found");
+  });
+});
---
+++ src/category/application/delete-category-use-case.ts
@@ -0,0 +1,11 @@
+import { IUseCase } from "../../shared/application/use-case-interface";
+import { UUID } from "../../shared/domain/value-object/uuid.value-object";
+import { ICategoryRepository } from "../domain/category-repository";
+
+export class DeleteCategoryUseCase implements IUseCase<string, void> {
+  constructor(private readonly categoryRepository: ICategoryRepository) {}
+
+  async execute(input: string): Promise<void> {
+    await this.categoryRepository.delete(new UUID(input));
+  }
+}
