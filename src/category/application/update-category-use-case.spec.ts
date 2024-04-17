fbf313f6476468b6718aa0518b6e043bbb9039f7
Micael Lima <5789811+mica-eu@users.noreply.github.com>
2024-04-16 22:18:05 -0300

    feat: add UpdateCategoryUseCase

---
+++ src/category/application/update-category-use-case.spec.ts
@@ -0,0 +1,140 @@
+import { each } from "lodash";
+import { UUID } from "../../shared/domain/value-object/uuid.value-object";
+import { Category } from "../domain/category-entity";
+import { ICategoryRepository } from "../domain/category-repository";
+import { InMemoryCategoryRepository } from "../infra/db/in-memory/in-memory-category-repository";
+import {
+  UpdateCategoryUseCase,
+  UpdateCategoryUseCaseInput,
+} from "./update-category-use-case";
+
+describe("UpdateCategoryUseCase", () => {
+  let categoryRepository: ICategoryRepository;
+  let updateCategoryUseCase: UpdateCategoryUseCase;
+
+  beforeEach(() => {
+    categoryRepository = new InMemoryCategoryRepository();
+    updateCategoryUseCase = new UpdateCategoryUseCase(categoryRepository);
+  });
+
+  it("updates a category", async () => {
+    jest.spyOn(InMemoryCategoryRepository.prototype, "find");
+    jest.spyOn(InMemoryCategoryRepository.prototype, "update");
+    const category = new Category({
+      id: new UUID(),
+      name: "Category Name",
+      description: "Category Description",
+      isActive: true,
+      createdAt: new Date(),
+    });
+    await categoryRepository.insert(category);
+    const input: UpdateCategoryUseCaseInput = {
+      id: category.id.value,
+      name: "New Category Name",
+      description: "New Category Description",
+      isActive: false,
+    };
+    await updateCategoryUseCase.execute(input);
+    expect(categoryRepository.find).toHaveBeenCalledTimes(1);
+    expect(categoryRepository.find).toHaveBeenCalledWith(category.id);
+    expect(categoryRepository.update).toHaveBeenCalledTimes(1);
+    expect(categoryRepository.update).toHaveBeenCalledWith(category);
+    expect((await categoryRepository.find(category.id))!.toJSON()).toEqual({
+      id: input.id,
+      name: input.name,
+      description: input.description,
+      isActive: input.isActive,
+      createdAt: category.createdAt,
+    });
+  });
+
+  it("updates category name", async () => {
+    jest.spyOn(InMemoryCategoryRepository.prototype, "find");
+    jest.spyOn(InMemoryCategoryRepository.prototype, "update");
+    const category = new Category({
+      id: new UUID(),
+      name: "Category Name",
+      description: "Category Description",
+      isActive: true,
+      createdAt: new Date(),
+    });
+    await categoryRepository.insert(category);
+    const input: UpdateCategoryUseCaseInput = {
+      id: category.id.value,
+      name: "New Category Name",
+    };
+    await updateCategoryUseCase.execute(input);
+    expect(categoryRepository.find).toHaveBeenCalledTimes(1);
+    expect(categoryRepository.find).toHaveBeenCalledWith(category.id);
+    expect(categoryRepository.update).toHaveBeenCalledTimes(1);
+    expect(categoryRepository.update).toHaveBeenCalledWith(category);
+    expect((await categoryRepository.find(category.id))!.toJSON()).toEqual({
+      id: input.id,
+      name: input.name,
+      description: category.description,
+      isActive: category.isActive,
+      createdAt: category.createdAt,
+    });
+  });
+
+  it("updates category description", async () => {
+    jest.spyOn(InMemoryCategoryRepository.prototype, "find");
+    jest.spyOn(InMemoryCategoryRepository.prototype, "update");
+    const category = new Category({
+      id: new UUID(),
+      name: "Category Name",
+      description: "Category Description",
+      isActive: true,
+      createdAt: new Date(),
+    });
+    await categoryRepository.insert(category);
+    const input: UpdateCategoryUseCaseInput = {
+      id: category.id.value,
+      description: "New Category Description",
+    };
+    await updateCategoryUseCase.execute(input);
+    expect(categoryRepository.find).toHaveBeenCalledTimes(1);
+    expect(categoryRepository.find).toHaveBeenCalledWith(category.id);
+    expect(categoryRepository.update).toHaveBeenCalledTimes(1);
+    expect(categoryRepository.update).toHaveBeenCalledWith(category);
+    expect((await categoryRepository.find(category.id))!.toJSON()).toEqual({
+      id: input.id,
+      name: category.name,
+      description: input.description,
+      isActive: category.isActive,
+      createdAt: category.createdAt,
+    });
+  });
+
+  it.each([true, false])(
+    "updates category isActive=%s",
+    async (isActive: boolean) => {
+      jest.spyOn(InMemoryCategoryRepository.prototype, "find");
+      jest.spyOn(InMemoryCategoryRepository.prototype, "update");
+      const category = new Category({
+        id: new UUID(),
+        name: "Category Name",
+        description: "Category Description",
+        isActive: true,
+        createdAt: new Date(),
+      });
+      await categoryRepository.insert(category);
+      const input: UpdateCategoryUseCaseInput = {
+        id: category.id.value,
+        isActive,
+      };
+      await updateCategoryUseCase.execute(input);
+      expect(categoryRepository.find).toHaveBeenCalledTimes(1);
+      expect(categoryRepository.find).toHaveBeenCalledWith(category.id);
+      expect(categoryRepository.update).toHaveBeenCalledTimes(1);
+      expect(categoryRepository.update).toHaveBeenCalledWith(category);
+      expect((await categoryRepository.find(category.id))!.toJSON()).toEqual({
+        id: input.id,
+        name: category.name,
+        description: category.description,
+        isActive: input.isActive,
+        createdAt: category.createdAt,
+      });
+    }
+  );
+});
---
+++ src/category/application/update-category-use-case.ts
@@ -0,0 +1,50 @@
+import { IUseCase } from "../../shared/application/use-case-interface";
+import { UUID } from "../../shared/domain/value-object/uuid.value-object";
+import { ICategoryRepository } from "../domain/category-repository";
+
+export interface UpdateCategoryUseCaseInput {
+  id: string;
+  name?: string;
+  description?: string;
+  isActive?: boolean;
+}
+
+export interface UpdateCategoryUseCaseOutput {
+  id: string;
+  name: string;
+  description: string | null;
+  isActive: boolean;
+  createdAt: Date;
+}
+
+export class UpdateCategoryUseCase
+  implements IUseCase<UpdateCategoryUseCaseInput, UpdateCategoryUseCaseOutput>
+{
+  constructor(private categoryRepository: ICategoryRepository) {}
+
+  async execute(
+    input: UpdateCategoryUseCaseInput
+  ): Promise<UpdateCategoryUseCaseOutput> {
+    const category = await this.categoryRepository.find(new UUID(input.id));
+    if (!category) {
+      throw new Error("Category not found");
+    }
+    if ("name" in input) {
+      category.changeName(input.name!);
+    }
+    if ("description" in input) {
+      category.changeDescription(input.description!);
+    }
+    if ("isActive" in input) {
+      input.isActive === true ? category.activate() : category.deactivate();
+    }
+    await this.categoryRepository.update(category);
+    return {
+      id: category.id.value,
+      name: category.name,
+      description: category.description,
+      isActive: category.isActive,
+      createdAt: category.createdAt,
+    };
+  }
+}
