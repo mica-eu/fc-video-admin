eccdba6eface9585ebf8b63ba93150156625decb
Micael Lima <5789811+mica-eu@users.noreply.github.com>
2024-04-20 21:10:14 -0300

    chore: add jest coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
-
 /** @type {import('jest').Config} */
 const config = {
   clearMocks: true,
   coverageProvider: "v8",
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
   },
 };
 
